12fcfb7aab71a420279348e18d4a4429650be3bf
Styles-theandriod <59735626+Styles-Theandriod@users.noreply.github.com>
2023-01-11 10:17:42 +0100

    Add files via upload

---
+++ strings.php
@@ -0,0 +1,25 @@
+<?php
+    //string length
+    echo strlen("hello world");
+    echo '<br>';
+
+    //string word count Output 2
+    echo str_word_count("Styles Theandriod");
+
+    echo "<br>";
+
+    // string reverse Output leunamme
+    echo strrev("emmanuel");
+
+
+    echo "<br>";
+
+    //string position OUtput Seven(7) 
+    echo strpos('Active window', 'window');
+
+    echo '<br>';
+
+    //string replace
+    echo str_replace("world", "globe", "hello world");
+
+?>
