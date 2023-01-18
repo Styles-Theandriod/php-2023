7ca0326283853f152ae1b1870ab3c6b4e2e238a0
Styles-theandriod <59735626+Styles-Theandriod@users.noreply.github.com>
2023-01-18 09:50:28 +0100

    Add files via upload

---
+++ arrays.php
@@ -0,0 +1,10 @@
+<?php
+// indexed array 
+// associative array 
+// multidimensional array 
+// sorting Array 
+
+// Normal Array 
+$fruits = array('mango', 'orange', 'pawpaw', 'pea');
+
+echo 'I like ' . $fruits[0] . ' ' . 'and' . ' '. $fruits[1] . ' ' . 'with' . ' ' . $fruits[2];
