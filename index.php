70ca711acf33d26a0ae98298fe3fa3d77164ee48
Styles-theandriod <59735626+Styles-Theandriod@users.noreply.github.com>
2023-01-26 10:09:45 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Read Update And Delete PHP MYSQL</title>
+</head>
+<body>
+
+    <?php
+        require('./config.php');
+    ?>
+    <!-- include_once
+    require
+    require_once -->
+    
+     <form action="" method="get">
+        <input type="text" name='name'>
+        <br>
+        <button type="submit">Submit</button>
+     </form>
+
+    <p> &copy 2023</p>
+</body>
+</html>
