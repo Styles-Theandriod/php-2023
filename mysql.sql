e2c1c1bd84017739aa2d8676da59f88e7789f200
Styles-theandriod <59735626+Styles-Theandriod@users.noreply.github.com>
2023-02-01 10:31:07 +0100

    Add files via upload

--- mysql.sql
+++ mysql.sql
@@ -1,3 +1,15 @@
 -- What is sql 
 -- sql stands for server query language 
 
+-- creating a new database in mysql 
+-- CREATE DATABASE annidb;
+
+-- Creating a table in mysql
+
+    CREATE TABLE `myemployees` (
+        `id` INT(11) NOT NULL AUTO_INCREMENT , 
+        `firstname` VARCHAR(100) NOT NULL , 
+        `lastname` VARCHAR(100) NOT NULL , 
+        `email` VARCHAR(100) NOT NULL , 
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB;
